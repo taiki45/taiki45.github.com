d7547d0c627af0af07142f69468b803a9d7cd1a5
Taiki ONO <taiks.4559@gmail.com>
2014-03-04 00:59:57 +0900

    Add blog

--- Gemfile
+++ Gemfile
@@ -3,11 +3,15 @@
 source 'https://rubygems.org'
 
 gem "middleman", "~>3.2.2"
-
-# Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
-
+gem "middleman-blog", "~> 3.5.2"
 gem "middleman-deploy"
+
+gem "redcarpet"
+gem "middleman-syntax"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
 
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,42 +1,47 @@
-###
-# Compass
-###
+# Time.zone = "UTC"
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+page "/", :layout => :about_layout
 
-###
-# Page options, layouts, aliases and proxies
-###
+activate :blog do |blog|
+  blog.name = 'blog'
+  blog.prefix = 'blog'
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
+  blog.layout = "article_layout"
+  blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  blog.year_link = "{year}.html"
+  blog.month_link = "{year}/{month}.html"
+  blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
 
-###
-# Helpers
-###
+page "/blog/feed.xml", layout: false
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+set :markdown_engine, :redcarpet
+set :markdown, :tables => true,
+               :autolink => true,
+               :gh_blockcode => true,
+               :fenced_code_blocks => true,
+               :with_toc_data => true,
+               :smartypants => true
+
+activate :syntax
 
 # Reload the browser automatically whenever files change
 activate :livereload
+
+activate :directory_indexes
 
 # middleman-deploy
 activate :deploy do |deploy|
@@ -45,32 +50,20 @@ activate :deploy do |deploy|
   deploy.branch = 'master'
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
